610e626efa6f6a2990d9d60c76d9b3d08613c115
Adrien B <adri141193@yahoo.fr>
2023-10-31 20:50:32 +0000

    complete testing ground

--- tests/test_datasets_diffusion.py
+++ tests/test_datasets_diffusion.py
@@ -8,6 +8,7 @@ from torch_geometric.loader import DataLoader
 
 from deepgraphgen.datasets_diffusion import (
     DatasetGrid,
+    NB_RANDOM_WALK
 )
 
 
@@ -16,12 +17,12 @@ from deepgraphgen.pl_trainer import TrainerGraphGDP
 
 def test_dataset_diffusion_grid():
     # init class for grid graph
-    dataset = DatasetGrid(nb_graphs=10, nx=10, ny=10, nb_timestep=1000)
+    dataset = DatasetGrid(nb_graphs=1000, nx=10, ny=10, nb_timestep=1000)
 
     assert dataset[0]["graph_noisy"].shape == (100, 100)
     assert dataset[0]["gradiant"].shape == (100, 100)
 
-    batch_size = 5
+    batch_size = 128
 
     # now we can test the dataloader
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -33,6 +34,9 @@ def test_dataset_diffusion_grid():
     assert batch["gradiant"].shape == (batch_size, 100, 100)
     assert batch["beta"].shape == (batch_size,)
 
-    model = TrainerGraphGDP(nb_layer=2, hidden_dim=16, nb_max_node=100)
+    model = TrainerGraphGDP(nb_layer=2, hidden_dim=16, nb_max_node=100,
+                            dim_edge=NB_RANDOM_WALK+1, dim_node=NB_RANDOM_WALK+1)
 
-    model.training_step(batch, 0)
+    for batch in dataloader:
+        model.training_step(batch, 0)
+        break
